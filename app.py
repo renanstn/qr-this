8381e569e732ad6bc49a5268ee3a20246e281bfe
renan <renan_ota@hotmail.com>
2025-07-21 18:16:29 -0300

    Create landing page

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/ping")
 def ping():
     return "<p>Pong!</p>"
+
+@app.route("/home")
+def home():
+    return render_template("index.html")
