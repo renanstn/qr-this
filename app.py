a4c561eb29cd7b59af0467d4e3c02cafcb6d2881
renan <renan_ota@hotmail.com>
2025-07-21 20:20:02 -0300

    Upload file feature working

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from bucket import (
+    MINIO_BUCKET_NAME,
+    minio_client,
+    create_bucket_if_not_exist,
+    upload_file,
+    get_minio_path,
+)
+
 
 app = Flask(__name__)
+
 
 @app.route("/ping")
 def ping():
     return "<p>Pong!</p>"
 
-@app.route("/home")
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    if request.method == "POST":
+        file = request.files["formFile"]
+        create_bucket_if_not_exist()
+        upload_file(file)
+        return "<p>Image uploaded!</p>"
+    else:
+        return render_template("index.html")
