025fdd28ba5ebb432a819fafe5ba440cbdce4ec9
renan <renan_ota@hotmail.com>
2025-07-23 19:41:49 -0300

    Fix start script again

--- minio/init_minio.sh
+++ minio/init_minio.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-set -e
 
-sleep 5
+set -e
+minio server /data --console-address ":9001" &
+MINIO_PID=$!
+sleep 10
 
 mc alias set local http://localhost:9000 "$MINIO_ROOT_USER" "$MINIO_ROOT_PASSWORD"
 mc mb --ignore-existing local/qr-app
 mc anonymous set download local/qr-app
 
-exec server /data --console-address ":9001"
+wait $MINIO_PID
